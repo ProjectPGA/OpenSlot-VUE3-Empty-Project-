bce1a740cef500160d24cce76d09d60526098fe6
Andresws12 <andreshernandez12.1995@gmail.com>
2022-08-30 22:51:36 +0200

    Init commit

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-strongly-recommended',
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-dupe-class-members': 'off', // handled by typescript
+    'no-use-before-define': 'off', // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-use-before-define.md
+    '@typescript-eslint/no-use-before-define': ['error'],
+  },
+  ignorePatterns: ['**/vite-env.d.ts'],
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
+  overrides: [
+    {
+      files: ['**/*.test.{j,t}s?(x)'],
+      env: {
+        jest: true,
+      },
+    },
+    {
+      files: ['*.js'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
+  ],
+};
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button class="button is-primary" type="button" @click="counter.increment">
+      count is {{ counter.count }}
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useCounterStore } from '../stores/counter';
+
+const counter = useCounterStore();
+
+defineProps<{ msg: string }>();
+</script>
+
+<style lang="scss" scoped></style>
